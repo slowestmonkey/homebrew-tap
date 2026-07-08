809c5979bc818a503e08aa1d9da1ce6cb9ef7ec9
Mark <mark.tsiareshka@gmail.com>
2026-07-08 19:04:05 +0200

    opendiff: update to v0.2.0

--- Formula/opendiff.rb
+++ Formula/opendiff.rb
@@ -1,8 +1,8 @@
 class Opendiff < Formula
   desc "Standalone terminal diff viewer for local git changes"
   homepage "https://github.com/slowestmonkey/opendiff"
-  url "https://github.com/slowestmonkey/opendiff/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "19a60b4fd180a6dc66816e11825a24c6a855449ebcf2944d6bf68c7fb6f39aa5"
+  url "https://github.com/slowestmonkey/opendiff/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "c901823d1571a2aa9dc2bb046be4cc03db1da6dbc270698aec98a9d788403737"
   license "MIT"
   head "https://github.com/slowestmonkey/opendiff.git", branch: "main"
 
